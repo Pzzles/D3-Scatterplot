a972645e8739ba1607b81f03f8b7e444a79c5b5b
Pule Tshetlha <tshetlhapulem@gmail.com>
2023-03-28 00:51:34 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,53 @@
+const url = 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json';
+const req = new XMLHttpRequest();
+let values =[];
+let xScale, yScale, xAxis, yAxis;
+const width = 800, height = 600, padding = 40;
+const svg = d3.select('svg'), tooltip = d3.select('#tooltip');
+
+const generateScales = () => {
+  xScale = d3.scaleLinear()
+             .domain([d3.min(values, item => item['Year']) - 1 , d3.max(values, item => item['Year']) + 1])
+             .range([padding, width-padding]);
+
+  yScale = d3.scaleTime()
+             .domain([d3.min(values, item => new Date(item['Seconds'] * 1000)), d3.max(values, item => new Date(item['Seconds'] * 1000))])
+             .range([padding, height-padding]);
+};
+
+const drawCanvas = () => svg.attr('width', width).attr('height', height);
+
+const drawPoints = () => {
+  svg.selectAll('circle')
+     .data(values)
+     .enter()
+     .append('circle')
+     .attr('class', 'dot')
+     .attr('r', '5')
+     .attr('data-xvalue', item => item['Year'])
+     .attr('data-yvalue', item => new Date(item['Seconds'] * 1000))
+     .attr('cx', item => xScale(item['Year']))
+     .attr('cy', item => yScale(new Date(item['Seconds'] * 1000)))
+     .attr('fill', item => item['URL'] === '' ? 'lightgreen' : 'orange')
+     .on('mouseover', item => {
+       tooltip.transition().style('visibility', 'visible');
+       tooltip.text(`${item['Year']} - ${item['Name']} - ${item['Time']} - ${item['Doping'] !== '' ? item['Doping'] : 'No Allegations'}`);
+       tooltip.attr('data-year', item['Year']);
+     })
+     .on('mouseout', () => tooltip.transition().style('visibility', 'hidden'));
+};
+
+const generateAxes = () => {
+  xAxis = d3.axisBottom(xScale).tickFormat(d3.format('d'));
+  yAxis = d3.axisLeft(yScale).tickFormat(d3.timeFormat('%M:%S'));
+
+  svg.append('g').call(xAxis).attr('id', 'x-axis').attr('transform', `translate(0, ${height-padding})`);
+  svg.append('g').call(yAxis).attr('id', 'y-axis').attr('transform', `translate(${padding}, 0)`);
+};
+
+req.open('GET', url, true);
+req.onload = () => {
+  values = JSON.parse(req.responseText);
+  drawCanvas(), generateScales(), drawPoints(), generateAxes();
+};
+req.send();
